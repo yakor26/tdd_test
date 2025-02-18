8b0922822b64fb0b3a8b21caf4a581f8c1c60ff9
Yetunde Korede <koredey@oregonstate.edu>
2025-02-18 01:28:42 -0500

    added loop to iterate and return False if no lower case found

--- check_pwd.py
+++ check_pwd.py
@@ -1,4 +1,13 @@
 def check_pwd(input_string):
     if len(input_string) < 8 or len(input_string) > 20:
         return False
+    count_lower = 0
+    digits = [str(x) for x in range (0,10)]
+    symbols = "~`!@#$%^&*()_+-="
+    for i in input_string:
+        if i == i.lower() and i not in digits and i not in symbols:
+            count_lower += 1
+
+    if count_lower == 0:
+        return False
     return True
