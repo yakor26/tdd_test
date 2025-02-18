a2b21ce41231ff180c6153126bfc1fb8985dc036
Yetunde Korede <koredey@oregonstate.edu>
2025-02-17 23:57:03 -0500

    wrote test2 to see if length greater than 20 is rejected

--- tests.py
+++ tests.py
@@ -5,6 +5,10 @@ class TestCase(unittest.TestCase):
     def test1(self):
         input = "abcdefg"
         self.assertFalse(check_pwd(input))
+    
+    def test3(self):
+        input = "abcdefgabcdefgabcdefg"
+        self.assertFalse(check_pwd(input))
 
 if __name__ == "__main__":
     unittest.main()
